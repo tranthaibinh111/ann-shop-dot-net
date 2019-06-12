4516c2614694bc2d0f3d7b61f251c87e12c3b2ec
Binh Tran <tranthaibinh111@yahoo.com>
2019-06-12 16:38:28 +0700

    Fix bug return IHttpActionResul

--- ann-shop-server/Services/PostService.cs
+++ ann-shop-server/Services/PostService.cs
@@ -12,21 +12,29 @@ namespace ann_shop_server.Services
         {
             using (var con = new inventorymanagementEntities())
             {
-                var data = con.tbl_Post.Where(x => x.ID == id).FirstOrDefault();
-
-                var category = con.tbl_PostCategory.Where(x => x.ID == data.CategoryID).FirstOrDefault();
-
-                PostModel post = new PostModel();
-
-                post.id = data.ID;
-                post.title = data.Title;
-                post.image = data.Image;
-                post.featured = data.Featured;
-                post.content = data.Content;
-                post.categoryName = category.Title;
-                post.categoryID = data.CategoryID;
-                post.createdDate = data.CreatedDate;
-
+                var post = con.tbl_Post
+                    .Where(x => x.ID == id)
+                    .GroupJoin(
+                        con.tbl_Category,
+                        p => p.CategoryID,
+                        c => c.ID,
+                        (p, c) => new { p, c }
+                    )
+                    .SelectMany(
+                        x => x.c.DefaultIfEmpty(),
+                        (parent, children) => new PostModel()
+                        {
+                            id = parent.p.ID,
+                            title = parent.p.Title,
+                            image = parent.p.Image,
+                            featured = parent.p.Featured,
+                            content = parent.p.Content,
+                            categoryName = children != null ? children.CategoryName : String.Empty,
+                            categoryID = parent.p.CategoryID,
+                            createdDate = parent.p.CreatedDate,
+                        }
+                    )
+                    .FirstOrDefault();
                 return post;
             }
         }
