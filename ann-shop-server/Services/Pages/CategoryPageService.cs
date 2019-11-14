dfc24376c016294c03510bf7da6fa539e6b18dbf
Binh Tran <tranthaibinh111@yahoo.com>
2019-11-14 14:01:09 +0700

    Add the preOrder API

--- ann-shop-server/Services/Pages/CategoryPageService.cs
+++ ann-shop-server/Services/Pages/CategoryPageService.cs
@@ -51,17 +51,35 @@ namespace ann_shop_server.Services
         }
 
         /// <summary>
-        /// Lấy tất cả sản phẩm thuộc nhánh slug
+        /// Lấy tất cả sản phẩm
         /// </summary>
         /// <param name="slug"></param>
         /// <param name="sort"></param>
         /// <param name="pagination"></param>
         /// <returns></returns>
-        public List<CategoryProductModel> getProduct(string slug, int sort, ref PaginationMetadataModel pagination)
+        private List<CategoryProductModel> getProduct(string slug, string preOrder, int sort, ref PaginationMetadataModel pagination)
         {
             using (var con = new inventorymanagementEntities())
             {
                 var source = con.tbl_Product.Where(x => x.WebPublish == true);
+
+                #region Lọc sản phẩm
+                #region Lọc sản phâm theo preOrder
+                if (!String.IsNullOrEmpty(preOrder))
+                {
+                    switch (preOrder)
+                    {
+                        case "hang-co-san":
+                            source = source.Where(x => x.PreOrder == false);
+                            break;
+                        case "hang-order":
+                            source = source.Where(x => x.PreOrder == true);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                #endregion
 
                 #region Lọc sản phẩm theo category
                 if (!String.IsNullOrEmpty(slug))
@@ -75,6 +93,7 @@ namespace ann_shop_server.Services
 
                     source = source.Where(x => categoryIDs.Contains(x.CategoryID.Value));
                 }
+                #endregion
                 #endregion
 
                 #region Tính toán số lượng có trong kho hàng
@@ -207,144 +226,51 @@ namespace ann_shop_server.Services
         }
 
         /// <summary>
-        /// Lấy tất cả sản phẩm có chứa từ khóa
+        /// Lấy tất cả sản phẩm theo slug category
         /// </summary>
         /// <param name="slug"></param>
+        /// <param name="sort"></param>
         /// <param name="pagination"></param>
         /// <returns></returns>
-        public List<CategoryProductModel> getProduct(int sort, ref PaginationMetadataModel pagination)
+        public List<CategoryProductModel> getProductByCategory(string slug, int sort, ref PaginationMetadataModel pagination)
         {
-            using (var con = new inventorymanagementEntities())
-            {
-                var source = con.tbl_Product.Where(x => x.WebPublish == true);
+            return this.getProduct(slug, String.Empty, sort, ref pagination);
+        }
 
-                #region Tính toán số lượng có trong kho hàng
-                var stockFilter = con.tbl_StockManager
-                    .Join(
-                        source,
-                        s => s.ParentID,
-                        d => d.ID,
-                        (s, d) => s
-                    )
-                    .ToList();
-                var stocks = StockService.Instance.getQuantities(stockFilter);
-                #endregion
+        /// <summary>
+        /// Lấy tất cả sản phẩm có chứa từ khóa
+        /// </summary>
+        /// <param name="sort"></param>
+        /// <param name="pagination"></param>
+        /// <returns></returns>
+        public List<CategoryProductModel> getProductAll(int sort, ref PaginationMetadataModel pagination)
+        {
+            return this.getProduct(String.Empty, String.Empty, sort, ref pagination);
+        }
 
-                #region Xuất thông tin về sản phẩm
-                var products = source.Where(x => x.CategoryID.HasValue)
-                    .Join(
-                        con.tbl_Category,
-                        pro => pro.CategoryID.Value,
-                        cat => cat.ID,
-                        (p, c) => new
-                        {
-                            productID = p.ID,
-                            title = p.ProductTitle,
-                            sku = p.ProductSKU,
-                            avatar = p.ProductImage,
-                            regularPrice = p.Regular_Price.HasValue ? p.Regular_Price.Value : 0,
-                            retailPrice = p.Retail_Price.HasValue ? p.Retail_Price.Value : 0,
-                            availability = false,
-                            materials = p.Materials,
-                            webPublish = p.WebPublish.HasValue ? p.WebPublish.Value : false,
-                            webUpdate = p.WebUpdate,
-                            slug = p.Slug,
-                            preOrder = p.PreOrder
-                        }
-                    )
-                    .ToList();
+        /// <summary>
+        /// Lấy tất cả sản phẩm order theo slug category
+        /// </summary>
+        /// <param name="slug"></param>
+        /// <param name="preOrder"></param>
+        /// <param name="sort"></param>
+        /// <param name="pagination"></param>
+        /// <returns></returns>
+        public List<CategoryProductModel> getProductOrderByCategory(string slug, string preOrder, int sort, ref PaginationMetadataModel pagination)
+        {
+            return this.getProduct(slug, preOrder, sort, ref pagination);
+        }
 
-                var data = products
-                    .GroupJoin(
-                        stocks,
-                        pro => pro.productID,
-                        info => info.productID,
-                        (pro, info) => new { pro, info }
-                    )
-                    .SelectMany(
-                        x => x.info.DefaultIfEmpty(),
-                        (parent, child) => new { product = parent.pro, stock = child }
-                    )
-                    .Where(x =>
-                        x.product.preOrder ||
-                        (
-                            x.stock != null &&
-                            x.stock.quantity >= 5
-                        )
-                    )
-                    .Select(x => new
-                    {
-                        productID = x.product.productID,
-                        title = x.product.title,
-                        sku = x.product.sku,
-                        thumbnails = Thumbnail.getALL(x.product.avatar),
-                        regularPrice = x.product.regularPrice,
-                        retailPrice = x.product.retailPrice,
-                        availability = x.stock != null ? x.stock.availability : x.product.availability,
-                        materials = x.product.materials,
-                        webUpdate = x.product.webUpdate,
-                        slug = x.product.slug,
-                        preOrder = x.product.preOrder
-                    });
-                #endregion
-
-                #region Thực hiện sắp xếp sản phẩm
-                if (sort == (int)CategorySort.PriceAsc)
-                {
-                    data = data.OrderBy(o => o.regularPrice);
-                }
-                else if (sort == (int)CategorySort.PriceDesc)
-                {
-                    data = data.OrderByDescending(o => o.regularPrice);
-                }
-                else if (sort == (int)CategorySort.ModelNew)
-                {
-                    data = data.OrderByDescending(o => o.productID);
-                }
-                else if (sort == (int)CategorySort.ProductNew)
-                {
-                    data = data.OrderByDescending(o => o.webUpdate);
-                }
-                else
-                {
-                    data = data.OrderByDescending(o => o.webUpdate);
-                }
-                #endregion
-
-                #region Tính toán phân trang
-                // Lấy tổng số record sản phẩm
-                pagination.totalCount = data.Count();
-
-                // Calculating Totalpage by Dividing (No of Records / Pagesize)
-                pagination.totalPages = (int)Math.Ceiling(pagination.totalCount / (double)pagination.pageSize);
-
-                // Returns List of product after applying Paging
-                var result = data
-                    .Select(x => new CategoryProductModel()
-                    {
-                        id = x.productID,
-                        name = x.title,
-                        sku = x.sku,
-                        thumbnails = x.thumbnails,
-                        regularPrice = x.regularPrice,
-                        retailPrice = x.retailPrice,
-                        materials = x.materials,
-                        slug = x.slug,
-                        badge = x.preOrder ? ProductBadge.order : (x.availability ? ProductBadge.stockIn : ProductBadge.stockOut)
-                    })
-                    .Skip((pagination.currentPage - 1) * pagination.pageSize)
-                    .Take(pagination.pageSize)
-                    .ToList();
-
-                // if CurrentPage is greater than 1 means it has previousPage
-                pagination.previousPage = pagination.currentPage > 1 ? "Yes" : "No";
-
-                // if TotalPages is greater than CurrentPage means it has nextPage
-                pagination.nextPage = pagination.currentPage < pagination.totalPages ? "Yes" : "No";
-                #endregion
-
-                return result;
-            }
+        /// <summary>
+        /// Lấy tất cả sản phẩm order
+        /// </summary>
+        /// <param name="preOrder"></param>
+        /// <param name="sort"></param>
+        /// <param name="pagination"></param>
+        /// <returns></returns>
+        public List<CategoryProductModel> getProductOrderAll(string preOrder, int sort, ref PaginationMetadataModel pagination)
+        {
+            return this.getProduct(String.Empty, preOrder, sort, ref pagination);
         }
         #endregion
     }
